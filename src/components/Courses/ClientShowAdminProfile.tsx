f89a7f1b92ba0196608299dfeb58b614fd62569f
blacksmitinv <gitlabacc@blacksmith-innovation.com>
2024-05-20 01:27:12 +0800

    added admin profile
    added admin profile logic (missed out)

---
+++ src/components/Courses/ClientShowAdminProfile.tsx
@@ -0,0 +1,45 @@
+// src/components/ClientShowAdminProfile.tsx
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../firebase/firebase';
+import { AdminProfile } from '../Interface/interfaces';
+
+const ClientShowAdminProfile: React.FC = () => {
+  const [profile, setProfile] = useState<AdminProfile[]>([]);
+  const [loading, setLoading] = useState(false);
+  const adminProfileCollectionRef = collection(db, 'admin');
+
+  const fetchProfile = async () => {
+    setLoading(true);
+    const querySnapshot = await getDocs(adminProfileCollectionRef);
+    const profileData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id } as AdminProfile));
+    setProfile(profileData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchProfile();
+  }, []);
+
+  return (
+    <div className="profile-container">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="profiles">
+          {profile.map((admin) => (
+            <div key={admin.id} className="profile-card">
+              <img src={admin.profilePictureUrl} alt={`${admin.name}'s profile`} className="profile-picture" />
+              <div className="profile-details">
+                <h2>{admin.name}</h2>
+                <p>{admin.background}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ClientShowAdminProfile;
--- src/pages/client/ClientHome.tsx
+++ src/pages/client/ClientHome.tsx
@@ -1,29 +1,15 @@
 // src/pages/client/ClientHome.tsx
-import React, { useState, useEffect } from 'react';
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../../firebase/firebase';
+import React from 'react';
 import ClientCourses from '../../components/Courses/ClientCourse';
+import ClientShowAdminProfile from '../../components/Courses/ClientShowAdminProfile';
 
 const ClientHome: React.FC = () => {
-  const [adminProfile, setAdminProfile] = useState<{ name: string, description: string }>({ name: '', description: '' });
-
-  useEffect(() => {
-    const fetchProfile = async () => {
-      const profileDocRef = doc(db, "profiles", "adminProfile");
-      const docSnap = await getDoc(profileDocRef);
-      if (docSnap.exists()) {
-        setAdminProfile(docSnap.data() as { name: string, description: string });
-      }
-    };
-    fetchProfile();
-  }, []);
-
   return (
     <div>
       <h1>Welcome to Our Training Courses</h1>
-      <h2>Trainer: {adminProfile.name}</h2>
-      <p>{adminProfile.description}</p>
-      <ClientCourses />
+      <h2>Trainer:</h2>
+        <ClientShowAdminProfile />
+        <ClientCourses />
     </div>
   );
 };
